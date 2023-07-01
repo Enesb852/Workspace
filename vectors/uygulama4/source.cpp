471afc9cc611a5f74133d6cdd9c31b3918f03bdc
Enesb852 <enesbolat852@gmail.com>
2023-07-01 21:33:46 +0300

    vectors/uygulama4 üzerinde çalışıldı.

---
+++ vectors/uygulama4/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    std::vector<int> v5 = {1, 2, 3, 4, 5};
+
+    cout << v5.back() << endl; // vektörün son elemanına bu şekilde ulaşabiliriz.
+
+    return 0;
+}
