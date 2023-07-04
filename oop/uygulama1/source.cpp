617603cfb833a290055a6620dcd06d013b292acc
Enesb852 <enesbolat852@gmail.com>
2023-07-04 23:01:40 +0300

    nesne yönelimli programlama üzerinde çalışıldı

---
+++ oop/uygulama1/source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class Nokta
+{   
+    private:
+        int x, y;
+    public:
+        void degerAta(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        void ekranaYaz()
+        {
+            cout << "(" << x << "," << y << ")" << endl;
+        }
+
+        bool isOrigin()
+        {
+            if(x==0&&y==0)
+                return true;
+            else
+                return false;
+        }   
+};
+
+int main()
+{
+    Nokta n1;
+    n1.degerAta(2,2);
+
+    n1.ekranaYaz();
+
+    if(n1.isOrigin())
+        cout << "Baslangic noktasinda";
+    
+    return 0;
+}
